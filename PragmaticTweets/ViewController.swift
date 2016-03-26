858ccdd05c7b0393afe812555068358cafb08e62
Eric Trepanier <eric.trepanier@me.com>
2016-03-26 11:20:28 -0400

    Initial Commit

---
+++ PragmaticTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by Éric Trépanier on 16-03-26.
+//  Copyright © 2016 Eric Trepanier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
