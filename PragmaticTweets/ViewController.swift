8ddb12f7fd4eed6f6d9190a7afd758827c7bd5ac
Éric Trépanier <eric_trepanier@apple.com>
2016-03-27 11:49:39 -0400

    Completed chapter 5, Connecting the UI to Code

--- PragmaticTweets/ViewController.swift
+++ PragmaticTweets/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var twitterWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleShowMyTweetsTapped(sender: UIButton) {
+        guard let url = NSURL(string: "https://twitter.com/eric_appleseed") else {
+            return
+        }
+        let urlRequest = NSURLRequest(URL: url)
+        twitterWebView.loadRequest(urlRequest)
+    }
+    
+    @IBAction func handleTweetButtonTapped(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            tweetVC.setInitialText("I just finished the first project in iOS 9 SDK Development. #pragios9")
+            self.presentViewController(tweetVC, animated: true, completion: nil)
+        } else {
+            NSLog("Can't send tweet")
+        }
+    }
 
 }
 
